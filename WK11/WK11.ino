d32110f13b21a07013dcbe42d8a8a7e86e430b27
Bern0630 <39110140+Bern0630@users.noreply.github.com>
2018-05-09 11:46:58 +0800

    Add files via upload

---
+++ WK11/WK11.ino
@@ -0,0 +1,19 @@
+
+int LED_PIN1 = 13;
+int LED_PIN2 = 12;
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_PIN1, OUTPUT);
+  pinMode(LED_PIN2, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_PIN1,HIGH);
+  digitalWrite(LED_PIN2,LOW);
+  delay(500);               // wait for a second
+  digitalWrite(LED_PIN1,LOW);
+  digitalWrite(LED_PIN2,HIGH);
+  delay(500);                       // wait for a second
+}
